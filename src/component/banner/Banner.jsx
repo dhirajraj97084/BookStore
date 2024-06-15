2b2677cc8d166e90f4e786876e8a3cc8e108cb67
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-06-15 11:51:18 +0530

    first commit

---
+++ src/component/banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className=' h-screen'>
+      banner
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Banner from '../../component/banner/Banner'
+import Layout from '../../component/layout/Layout'
+
+
+function Home() {
+  return (
+    <Layout>
+      
+      <Banner/>
+      
+    </Layout>
+  )
+}
+
+export default Home
