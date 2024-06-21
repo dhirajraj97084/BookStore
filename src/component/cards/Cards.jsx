a802d34d80c8aebd279ff410f70915d79697217c
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-06-21 09:09:15 +0530

    okay

--- src/component/cards/Cards.jsx
+++ src/component/cards/Cards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Cards({item}) {
     return (
         <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96 bg-base-100 shadow-md my-8">
                 <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">
